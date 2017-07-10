1b889ab334b7ede5d0518d65a9fd43952fdd0c04
rima <rima@latitude>
2017-07-10 13:43:24 +0200

    pierwszyComit

---
+++ public_html/newjavascript.js
@@ -0,0 +1,122 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/*var btnMenu = document.getElementById("btn-menu");
+btnMenu.addEventListener("click", toggleMenu);
+function toggleMenu(e){
+    var divOverlay = document.getElementsByClassName("overlay")[0];
+    var speed = 10;
+    var eventTarget = e.target;
+    if( eventTarget.className == "btn-open" ){
+        fadeIn(divOverlay, speed);
+        eventTarget.className = "btn-close";
+    } else if ( eventTarget.className == "btn-close"){
+                fadeOut(divOverlay, speed);
+
+        eventTarget.className = "btn-open";
+    }
+}
+
+function fadeIn(elem, speed){
+    
+    var inInterval = setInterval(function(){
+        elem.style.opacity = Number(elem.style.opacity) + 0,02;
+        if (elem.style.opacity >= 1) {
+            elem.style.opacity = 1;
+            clearInterval(inInterval);
+        }
+             
+                
+        
+    }, speed);
+}
+
+function fadeOut(elem, speed){
+    
+    var outInterval = setInterval(function(){
+        elem.style.opacity = Number(elem.style.opacity) - 0,02;
+        if (elem.style.opacity <= 0 ) {
+            elem.style.opacity = 0;
+            clearInterval(outInterval);
+        }
+             
+                
+        
+    }, speed);
+}*/
+
+var btnMenu = document.getElementById("btn-menu");
+btnMenu.addEventListener("click", toggleMenu);
+
+
+function toggleMenu(e) {
+    
+    var divOverlay = document.getElementsByClassName("overlay")[0];
+    var speed = 10;
+    
+    
+    var eventTarget = e.target;
+    
+    if( eventTarget.className == "btn-open" ) {
+        fadeIn(divOverlay, speed);
+        eventTarget.className = "btn-close";
+        
+    } else if ( eventTarget.className == "btn-close") {
+        fadeOut(divOverlay, speed);
+        eventTarget.className = "btn-open";
+    }
+    
+}
+
+function fadeIn(elem, speed) {
+    
+    var inInterval = setInterval(function(){
+        
+    elem.style.opacity = Number(elem.style.opacity) + 0.02;
+    
+    if (elem.style.opacity >= 1) {
+        
+        elem.style.opacity = 1;
+        clearInterval(inInterval);
+        
+    }    
+        
+        
+    }, speed); // 10ms == .01s
+    
+    
+    
+}
+
+function fadeOut(elem, speed) {
+    
+    var outInterval = setInterval(function(){
+        
+    elem.style.opacity = Number(elem.style.opacity) - 0.02;
+    
+    if (elem.style.opacity <= 0) {
+        
+        elem.style.opacity = 0;
+        clearInterval(outInterval);
+        
+    }    
+        
+        
+    }, speed); // 10ms == .01s
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
